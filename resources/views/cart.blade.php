4078acf83871b7b7b0742fba8ca6562967dabfd8
Biswarup Banerjee <bis.banerjee.bb@gmail.com>
2019-05-06 19:46:52 +0530

    added layout

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,31 +1,5 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-    span:hover {
-        background: rgba(0, 0, 0, 0.2);
-    }
-    a {
-        color: white !important;
-        text-decoration: none !important;
-    }
-</style>
-<div class="navbar-top" style="height: 55px;   width: 100%; top : 0px; background-color: #0F69D9">
-<span style="font-size: 30px; color: white; padding: 8px;">
-    BuyPet.com
-</span>
-<span style="font-size: 20px; color: white; padding: 15px;  cursor: pointer; margin-left: 200px; ">
-<a  href="/shop">Shop</a>
-</span>
-<span style="font-size: 20px; color: white; padding: 15px; cursor: pointer ; margin-left: 200px;">
-    <a  href="/cart">My Cart</a>
-
-</span>
-<span style="font-size: 20px; color: white; padding: 15px;  cursor: pointer ; margin-left: 200px;">
-<a  href="/addItem">Sell Your Pet</a>
-</span>
-
-</div>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><nav class="navbar navbar-default">
-
+@extends('navbar')
+@section('content')
 <div class="container"  style="margin-top: 50px">
 <div class="row" style="">
 <br/>
@@ -80,3 +54,4 @@ $count = 0;
   </div>
 </div>
 </div>
+@endsection
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,31 +1,5 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-    span:hover {
-        background: rgba(0, 0, 0, 0.2);
-    }
-    a {
-        color: white !important;
-        text-decoration: none !important;
-    }
-</style>
-<div class="navbar-top" style="height: 55px;   width: 100%; top : 0px; background-color: #0F69D9">
-<span style="font-size: 30px; color: white; padding: 8px;">
-    BuyPet.com
-</span>
-<span style="font-size: 20px; color: white; padding: 15px;  cursor: pointer; margin-left: 200px; ">
-<a  href="/shop">Shop</a>
-</span>
-<span style="font-size: 20px; color: white; padding: 15px; cursor: pointer ; margin-left: 200px;">
-    <a  href="/cart">My Cart</a>
-
-</span>
-<span style="font-size: 20px; color: white; padding: 15px;  cursor: pointer ; margin-left: 200px;">
-<a  href="/addItem">Sell Your Cat</a>
-</span>
-
-</div>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><nav class="navbar navbar-default">
-
+@extends('navbar')
+@section('content')
 <ul class="nav nav-pills nav-fill" style="width: 100%; color: black;">
   <li class="nav-item" style="margin: 20px">
   <form method="GET" action="/shop/dog">
@@ -85,3 +59,4 @@
 
 @endforeach
 </div>
+@endsection
